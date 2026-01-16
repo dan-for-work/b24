6b4404429e23bbbb75a7d5653e1204bb3c0d30cd
dan <dan.for.works@gmail.com>
2026-01-16 10:53:11 +0300

    remove unused

--- netlify/functions/utils/errors.js
+++
@@ -1,6 +0,0 @@
-export class SkipExecution extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "SkipExecution";
-  }
-}
--- netlify/functions/utils/response.js
+++
@@ -1,7 +0,0 @@
-export function ok(message) {
-  return new Response(message, { status: 200 });
-}
-
-export function error(message, status = 500) {
-  return new Response(message, { status });
-}
